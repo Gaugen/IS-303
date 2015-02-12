496f83e706eb07c3bdf52515525cd9fb05cacf58
Gaugen <torabang@gmail.com>
2015-02-12 14:19:38 +0100

    Laget en minimal app med mulighet til å hentedata

---
+++ Nettbeans/Forferdelig.no/src/AppPackage/HomeGUI.java
@@ -0,0 +1,204 @@
+
+package AppPackage;
+
+import com.teknikindustries.yahooweather.WeatherDisplay;
+import com.teknikindustries.yahooweather.WeatherDoc;
+
+
+public class HomeGUI extends javax.swing.JFrame {
+    private String cityName; 
+
+   
+    public HomeGUI() {
+        initComponents();
+        getWeather();
+        getWeatherSearch();
+    }
+
+    private void getWeather()
+    {
+        WeatherDoc doc = new WeatherDoc("858537","c");
+        WeatherDisplay disp = new WeatherDisplay();
+      //  System.out.println(disp.getTemperature());
+        Temperature.setText(disp.getTemperature() + disp.getTemperatureUnit());
+        City.setText(disp.getCity());
+        Country.setText(disp.getCountry());
+    }
+    
+    private void setCityName(String SearchText)
+    {
+        this.cityName = SearchText;
+    }
+    
+    private void getWeatherSearch()
+    {
+        WeatherDoc doc = new WeatherDoc("SearchText","c");
+        WeatherDisplay disp = new WeatherDisplay();
+        Country2.setText(disp.getCountry());
+        City2.setText(disp.getCity());
+        Temperature2.setText(disp.getTemperature() + disp.getTemperatureUnit());
+    }
+  
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        Temperature = new javax.swing.JLabel();
+        City = new javax.swing.JLabel();
+        Country = new javax.swing.JLabel();
+        SearchText = new java.awt.TextField();
+        SearchButton = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        Country2 = new javax.swing.JLabel();
+        City2 = new javax.swing.JLabel();
+        Temperature2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        Temperature.setText("Temperature");
+
+        City.setText("City");
+
+        Country.setText("Country");
+
+        SearchText.setText("textField1");
+        SearchText.addTextListener(new java.awt.event.TextListener() {
+            public void textValueChanged(java.awt.event.TextEvent evt) {
+                SearchTextTextValueChanged(evt);
+            }
+        });
+        SearchText.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SearchTextActionPerformed(evt);
+            }
+        });
+
+        SearchButton.setText("Search!");
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jTextArea1.setText("Here you can search\nfor the weather. You can \nchoose between displaying \nthe data in metric or ");
+        jScrollPane1.setViewportView(jTextArea1);
+
+        Country2.setText("Country");
+
+        City2.setText("City");
+
+        Temperature2.setText("Temperature");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(Temperature)
+                            .addComponent(City))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(Country)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(SearchButton)
+                        .addGap(54, 54, 54))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(Country2)
+                            .addComponent(SearchText, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(City2)
+                            .addComponent(Temperature2))
+                        .addGap(40, 40, 40))))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(93, 93, 93)
+                        .addComponent(Country)
+                        .addGap(16, 16, 16)
+                        .addComponent(City)
+                        .addGap(18, 18, 18)
+                        .addComponent(Temperature))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(Country2)
+                .addGap(18, 18, 18)
+                .addComponent(City2)
+                .addGap(18, 18, 18)
+                .addComponent(Temperature2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
+                .addComponent(SearchText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(25, 25, 25)
+                .addComponent(SearchButton)
+                .addGap(47, 47, 47))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void SearchTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SearchTextActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_SearchTextActionPerformed
+
+    private void SearchTextTextValueChanged(java.awt.event.TextEvent evt) {//GEN-FIRST:event_SearchTextTextValueChanged
+        // TODO add your handling code here:
+    }//GEN-LAST:event_SearchTextTextValueChanged
+
+   
+    public static void main(String args[]) {
+       
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(HomeGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(HomeGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(HomeGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(HomeGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+       
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new HomeGUI().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel City;
+    private javax.swing.JLabel City2;
+    private javax.swing.JLabel Country;
+    private javax.swing.JLabel Country2;
+    private javax.swing.JButton SearchButton;
+    private java.awt.TextField SearchText;
+    private javax.swing.JLabel Temperature;
+    private javax.swing.JLabel Temperature2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    // End of variables declaration//GEN-END:variables
+}
