53b7bb374cc2f48e194700e48e874d876c0d664d
Gaugen <torabang@gmail.com>
2015-03-04 16:33:52 +0100

    jobbet med app

--- Kode/pyweather3.py
+++ Kode/pyweather3.py
@@ -1,30 +1,3 @@
-"""
-
-Copyright (c) 2013 Jorick A. Caberio and contributors
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-
-documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-
-the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
-
-to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
-
-the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
-
-THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
-
-OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
-
-OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-"""
-
-
 import json,  urllib2, Tkinter, tkSimpleDialog, tkMessageBox
 
 def fetchHTML(url):
@@ -33,6 +6,9 @@ def fetchHTML(url):
     req = urllib2.Request(URL)
     response=urllib2.urlopen(req)
     return response.read()
+def cancel():
+    if tkMessageBox.askokcancel("Exit", "Are you sure?"):
+        root.destroy()
 
 root = Tkinter.Tk()
 root.withdraw()
@@ -59,4 +35,8 @@ tkMessageBox.showinfo("Results", "Location: " + str(data['name']) +
 "\n\nPressure: " + str(data['main']['pressure']) + " hPa"+
 "\n\nWind Speed: " + str(data['wind']['speed']) + " mps" +
 "\n\nWeather Description: " + str(data['weather'][0]['description']))
+
+tkMessageBox.cancel("Exit", "Are you sure?")
+
+
 
